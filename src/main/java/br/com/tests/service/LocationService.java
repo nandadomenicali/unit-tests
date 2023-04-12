73306f37726ec489cf7b9ded24d68ef01f2e4290
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-04-12 14:53:07 -0300

    Adding junit dependency and implementing class teste

--- src/main/java/br/com/tests/service/LocationService.java
+++ src/main/java/br/com/tests/service/LocationService.java
@@ -4,6 +4,7 @@ import br.com.tests.entity.Location;
 import br.com.tests.entity.Movie;
 import br.com.tests.entity.User;
 import br.com.tests.utils.DataUtils;
+import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
@@ -25,19 +26,5 @@ public class LocationService {
         //TODO Add method to save location
 
         return location;
-    }
-
-    public static void main(String[] args) {
-        // testing without framework
-        LocationService locationService = new LocationService();
-        User user = new User("MockedUser");
-        Movie movie = new Movie("MockedMovie", 2, 5.0);
-
-        Location location = locationService.rentMovie(user, movie);
-
-        System.out.println(location.getPrice() == 5.0);
-        System.out.println(DataUtils.isTheSameDate(location.getLocationDate(), new Date()));
-        System.out.println(DataUtils.isTheSameDate(location.getReturnDate(),
-                DataUtils.getDaysWithDiffDays(1)));
     }
 }
---
+++ src/test/java/LocationServiceTest.java
@@ -0,0 +1,26 @@
+import br.com.tests.entity.Location;
+import br.com.tests.entity.Movie;
+import br.com.tests.entity.User;
+import br.com.tests.service.LocationService;
+import br.com.tests.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocationServiceTest {
+
+    @Test
+    public void test() {
+        LocationService locationService = new LocationService();
+        User user = new User("MockedUser");
+        Movie movie = new Movie("MockedMovie", 2, 5.0);
+
+        Location location = locationService.rentMovie(user, movie);
+
+        Assert.assertTrue(location.getPrice() == 5.0);
+        Assert.assertTrue(DataUtils.isTheSameDate(location.getLocationDate(), new Date()));
+        Assert.assertTrue(DataUtils.isTheSameDate(location.getReturnDate(),
+                DataUtils.getDaysWithDiffDays(1)));
+    }
+}
